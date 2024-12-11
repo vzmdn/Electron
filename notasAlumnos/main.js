9ee010ea954f81b9c959495167413dbcbc5d8e28
vzmdn <carlosvozmedianoruiz@gmail.com>
2024-12-11 13:04:23 +0100

    notasAlumnos

---
+++ notasAlumnos/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.handle('show-dialog', async (event, args) => {
+  const result = await dialog.showMessageBox({
+    type: 'info',
+    title: '',
+    message: ''
+  });
+  return result;
+});
