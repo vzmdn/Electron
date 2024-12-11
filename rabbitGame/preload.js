facf93718fae2370d820ed087a78fff9c6747e8b
Carlos Vozmediano <carlosvozmedianoruiz@gmail.com>
2024-12-11 20:44:00 +0100

    Moved rabbitGame to another repository

--- rabbitGame/preload.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer, contextBridge } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    showMessageBox: (message) => {
-        return ipcRenderer.invoke("show-message-box", message);
-    },
-});
